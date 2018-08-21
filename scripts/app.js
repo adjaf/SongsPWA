5bf8ec5d21b9a2a1f5704abfd68753fe6d9cf010
Antonio Avila <antonioavifernan@gmail.com>
2018-08-20 21:57:21 -0500

    added 512x png

--- scripts/app.js
+++ scripts/app.js
@@ -8,13 +8,9 @@
     };
 
     /*
-     *
      *  EVENT LISTENERS
-     * 
      * */
-
     document.getElementById('btnRefresh').addEventListener('click',function(){
-        console.log("entro");
         app.loadSongs();
     });
 
@@ -76,16 +72,12 @@
     // Startup Code
     app.init();
 
-    if (navigator.serviceWorker.controller) {
-        console.log('[PWA Builder] active service worker found, no need to register')
-      } else {
-      
-      //Register the ServiceWorker
-        navigator.serviceWorker.register('service-worker.js', {
-          scope: './'
-        }).then(function(reg) {
-          console.log('Service worker has been registered for scope:'+ reg.scope);
-        });
-      }
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register("./service-worker.js")
+            .then(function(registration) {
+                console.log('Service Worker Registered', registration.scope);
+            });
+    }
 
 })();
